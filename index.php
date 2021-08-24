b429af368c08e8f4cbb754cd99c482bc59ac1fff
trinidad1994 <88596846+trinidad1994@users.noreply.github.com>
2021-08-24 18:28:34 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+function obtenerIP () {
+   if ( filter_var( $_SERVER['HTTP_CLIENT_IP'], FILTER_VALIDATE_IP ) ) {
+      return $_SERVER['HTTP_CLIENTE_IP'];
+   }
+   elseif ( filter_var ( $_SERVER['HTTP_X_FORWARDED_FOR'], FILTER_VALIDATE_IP ) ) {
+      return $_SERVER['HTTP_X_FORWARDED_FOR'];
+   }
+   elseif ( filter_var ( $_SERVER['HTTP_VIA'], FILTER_VALIDATE_IP ) ) {
+      return $_SERVER['HTTP_VIA'];
+   }
+   else {
+      return $_SERVER['REMOTE_ADDR'];
+   }
+?>
